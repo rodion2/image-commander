38d63f9d5999d76f41f8c8885b8c87adc1a1af12
Rodion Minkin <rodion44342@gmail.com>
2022-06-16 00:03:38 +0300

    * updated constants

--- src/app/constants/app-constants.ts
+++ src/app/constants/app-constants.ts
@@ -1,6 +1,8 @@
 export class AppConstants {
-   public static BASE_SERVER_URI = "http://localhost:8080/";
-   public static SEARCH_URI = "search/";
-   public static GET_IMAGES_URI = "images"
-   public static BASE_64_PREFIX = "data:image/jpeg;base64,";
+  public static BASE_SERVER_URI = "http://localhost:8080/";
+  public static SEARCH_URI = "search/";
+  public static GET_IMAGES_URI = "images"
+  public static UPLOAD_PHOTO_URI = "object/upload/";
+  public static OBJECT_URI = "object";
+  public static BASE_64_PREFIX = "data:image/jpeg;base64,";
 }
--- src/app/photo-saver/photo-c-r-u-d.service.ts
+++ src/app/photo-saver/photo-c-r-u-d.service.ts
@@ -3,35 +3,33 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {ImageDto} from "../model/image-dto";
 import {S3UploadResult} from "../model/s3-upload-result";
+import {AppConstants} from "../constants/app-constants";
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotoCRUDService {
-  private UPLOAD_PHOTO_URI = "object/upload/";
-  private OBJECT_URI = "object";
-  private BASE_SERVER_URI = "http://localhost:8080/";
 
 
   constructor(private httpClient: HttpClient) {
   }
 
   getPhotoContent(fileName: String): Observable<ImageDto> {
-    return this.httpClient.get<ImageDto>(this.BASE_SERVER_URI + this.OBJECT_URI + "/?object-name=" + fileName);
+    return this.httpClient.get<ImageDto>(AppConstants.BASE_SERVER_URI + AppConstants.OBJECT_URI + "/?object-name=" + fileName);
   }
 
   getPhotos(): Observable<ImageDto[]> {
-    return this.httpClient.get<ImageDto[]>(this.BASE_SERVER_URI + this.OBJECT_URI + "/list");
+    return this.httpClient.get<ImageDto[]>(AppConstants.BASE_SERVER_URI + AppConstants.OBJECT_URI + "/list");
   }
 
   uploadPhoto(file: File): Observable<S3UploadResult> {
     const uploadImageData = new FormData();
     uploadImageData.append('file', file);
-    return this.httpClient.post<S3UploadResult>(this.BASE_SERVER_URI + this.UPLOAD_PHOTO_URI, uploadImageData);
+    return this.httpClient.post<S3UploadResult>(AppConstants.BASE_SERVER_URI + AppConstants.UPLOAD_PHOTO_URI, uploadImageData);
   }
 
   deletePhoto(fileName: String): Observable<any> {
-    return this.httpClient.delete(this.BASE_SERVER_URI + this.OBJECT_URI + "/" + encodeURI(fileName.toString()), {responseType: 'text'});
+    return this.httpClient.delete(AppConstants.BASE_SERVER_URI + AppConstants.OBJECT_URI + "/" + encodeURI(fileName.toString()), {responseType: 'text'});
   }
 
 
